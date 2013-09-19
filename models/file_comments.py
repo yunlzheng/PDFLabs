87629865370109cd821574fa03693526304eb138
zhengyl <zhengyl@yun70.com.cn>
2013-09-19 12:39:24 +0800

    update resources

--- models/file_comments.py
+++
@@ -1,9 +0,0 @@
-# -*- coding : utf-8 -*-
-
-from mongoengine import *
-from models.users import User
-
-class FileComment(EmbeddedDocument):
-    content = StringField()
-    name = StringField(max_length=120)
-    author = ReferenceField(User)
