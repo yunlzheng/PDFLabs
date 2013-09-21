03cdc781620a367b1f5f410323f1084e96cba39e
zhengyl <zhengyl@yun70.com.cn>
2013-09-21 13:48:12 +0800

    close use api

--- models/__init__.py
+++ models/__init__.py
@@ -5,4 +5,3 @@ from models.admin import Admin as admin
 from models.books import Book as book
 from models.groups import Group as group
 from models.posts import Post as post
-from models.users import User as user
