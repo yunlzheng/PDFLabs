1bf4097db389b3c3137cc9ee2c259b334117a96d
zhengyl <zhengyl@yun70.com.cn>
2013-09-21 21:56:08 +0800

    update

--- static/js/module/logs.js
+++ static/js/module/logs.js
@@ -1,7 +1,7 @@
 define(["jquery","application", "module/ajax_client" ], function($,application,client) {
     //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
     $(function() {
-    	application.navigationTo('logs');
+    	
     	/*initialize()*/
     });
 
