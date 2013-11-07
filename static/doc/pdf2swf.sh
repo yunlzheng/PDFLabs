d71797622edba1207505c3f63e04e416cbd79537
zhengyl <zhengyl@yun70.com.cn>
2013-11-06 18:00:07 -0800

    调整项目静态资源结构

--- static/doc/pdf2swf.sh
+++
@@ -1,1 +0,0 @@
-pdf2swf $1.pdf -o $1.swf -f -T 9 -t -s storeallcharacters
--- static/qrcode/code.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-import qrcode
-
-text = raw_input('enter book id:')
-
-
-img = qrcode.make("cuttle.herokuapp.com/book/"+text)
-img.save(text+'.png')
-
